e30df68922f25450bd28f5e34061f83e3f4fb663
annih068-boop <annih068@gmail.com>
2026-09-11 15:31:20 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// 只填写 Supabase Project URL 和 anon public key。
+// anon public key 可以放在前端；service_role key 绝对不能放进这里。
+window.SUPABASE_CONFIG = {
+  url: 'https://pnxhczlbqglxcgydncwi.supabase.co',
+  anonKey: 'sb_publishable_yA8DHoeXu5O4qteqRPe7YQ_k0e-tTwj'
+};
