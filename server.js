b8c52fc4f7a2d50f7a71aed35b4e783cde04d528
annih068-boop <annih068@gmail.com>
2026-09-11 15:35:39 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/supabase-config.js', (request, response) => {
+  const url = process.env.SUPABASE_URL || '';
+  const anonKey = process.env.SUPABASE_ANON_KEY || '';
+  response.type('application/javascript').send(`window.SUPABASE_CONFIG = ${JSON.stringify({ url, anonKey })};`);
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (request, response) => {
